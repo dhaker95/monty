09c8b9afcb067204cccd2f9f757ad481bc3d9d46
Dhaker Achour <achour.dhaker@gmail.com>
2023-01-19 00:23:09 -0800

    NOP

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - it doesn't do anything
+ * @head: a pointer to the stack
+ * @line_number: the number of lines readed
+ *
+ */
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
